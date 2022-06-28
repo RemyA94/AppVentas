a2ee633efe0414a3358f46e5def9eca6fec36407
RemyA94 <remyjoseacostamelo@gmail.com>
2022-06-27 21:56:11 -0400

    ✏️

---
+++ CapaPresentacionAdmin/Recursos/EnviarCorreoUsuarios.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace CapaPresentacionAdmin.Recursos
+{
+    public class EnviarCorreoUsuarios
+    {
+
+        public static bool EnviarCorreo(string correo, string asunto, string mensaje) 
+        {
+            bool resultado = false;
+            try 
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(correo);
+                mail.From = new MailAddress("igrismarshalshadow@gmail");
+                mail.Subject = asunto;
+                mail.Body = mensaje;
+                mail.IsBodyHtml = true;
+
+                var smtp = new SmtpClient()
+                {
+                    Credentials = new NetworkCredential("igrismarshalshadow@gmail", "fdyykwfumlzgmjda"),
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true
+                };
+                smtp.Send(mail);
+                resultado = true;
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                resultado = false;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ CapaPresentacionAdmin/Recursos/GenerarClaveUsuario.cs
@@ -0,0 +1,11 @@
+﻿namespace CapaPresentacionAdmin.Recursos
+{
+    public class GenerarClaveUsuario
+    {
+        public static string GenerarClave()
+        {
+            string clave = Guid.NewGuid().ToString( "N").Substring(1,6);
+            return clave;
+        }
+    }
+}
